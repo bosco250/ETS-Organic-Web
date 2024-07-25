671bab81f072b1478ef4115502e68c37c90030c9
bosco250 <dusengimana06@gmail.com>
2024-07-25 10:16:03 +0200

    changes

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -3,21 +3,21 @@ import { IoIosStar} from 'react-icons/io'
 
 const ProductCard = (props) => {
   return (
-    <div className="flex flex-col w-[250px] ">
+    <div className="flex flex-col w-[250px] bg-[#DEDDDD] rounded-xl">
       <div className="">
       <button className="rounded-md p-1 text-white bg-[#274C5B]"> {props.category}</button>
       </div>
       <div className="">
         <img src={props.image} className=' w-full h-[270px]' />
       </div>
-      <div className="flex flex-col">
-        <p className="font-roboto text-[20px] font-[500] text-[#274C5B] ">{props.name} </p>
+      <div className="flex flex-col px-5 bg-[#DEDDDD] rounded-xl">
+        <p className="font-roboto text-[17px] font-[500] text-[#274C5B] bg-[#DEDDDD] ">{props.name} </p>
         <div className="flex justify-between items-center">
           <div className="flex gap-3 mt-2">
           <p className=" line-through text-gray-400">$20.00</p>
-          <p className="font-sans font-[600] text-[18px] text-[#274C5B] ">{props.cost}</p>            
+          <p className="font-sans font-[600] text-[14px] text-[#274C5B] ">{props.cost}</p>            
           </div>
-          <div className=" flex flex-row gap-1 text-white ">
+          <div className=" flex flex-row gap-1 text-white w-1/3">
           <IoIosStar className=' text-orange-500 fill-orange-500'/>
           <IoIosStar className=' text-orange-500 fill-orange-500'/>
           <IoIosStar className=' text-orange-500 fill-orange-500'/>
